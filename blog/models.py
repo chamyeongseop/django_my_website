ec21baa23443fc60f7f37c452aaed75dd2ae6496
chamyeongseop <chamysub@postech.ac.kr>
2020-03-18 03:40:45 +0900

    TIL

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=30) # 길이 제한이 있음
+    content = models.TextField()
+
+    created = models.DateTimeField()
+    author = models.ForeignKey(User)
